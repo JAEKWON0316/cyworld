0e322e3200c8b076e5d14520ffc91bbfce7c1261
재권 <diwo02@naver.com>
2024-10-14 18:29:27 +0900

    push

---
+++ src/main/java/com/cyworld/cyworldProject/CyworldProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cyworld.cyworldProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyworldProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyworldProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyworld/cyworldProject/CyworldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyworld.cyworldProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyworldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
