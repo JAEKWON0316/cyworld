af16a56e52c3aba8db5799a555aa3e47f33063c7
yesun1234 <dptjs1106@naver.com>
2024-10-20 16:45:45 +0900

    ddd

--- cyworld-react/src/App.js
+++ cyworld-react/src/App.js
@@ -6,14 +6,17 @@ import Board from './nav/Board';
 import Guest from './nav/Guest';
 import Photo from './nav/Photo';
 import Profile from './nav/Profile';
-import Diary  from './nav/DiaryList';
 import Nav from './component/Nav';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import PhotoWrite from './nav/PhotoWrite';
 import PhotoReWrite from './nav/PhotoReWrite';
+import Main from './nav/Main';
+import Join from './nav/Join';
+import Find from './nav/Find';
 
-const App = () => {
-  const [showPhotoAlbum, setShowPhotoAlbum] = useState(false); // State for controlling photo album visibility
+const AppContent = () => {
+  const [showPhotoAlbum, setShowPhotoAlbum] = useState(false); 
+  const location = useLocation();
 
   useEffect(() => {
     const app = document.querySelector('.content');
@@ -22,39 +25,59 @@ const App = () => {
       console.log('Scrolled!', app.scrollTop);
     };
   
-    app.addEventListener('scroll', handleScroll);
+    if (app) {
+      app.addEventListener('scroll', handleScroll);
+    }
   
     return () => {
-      app.removeEventListener('scroll', handleScroll);
+      if (app) {
+        app.removeEventListener('scroll', handleScroll);
+      }
     };
   }, []);
-  
+
+  // Check if we are on the main page (root URL) or join page
+  const isMainPage = location.pathname === '/' || location.pathname === '/join' || location.pathname === '/find';
+
+  return (
+    <div className='app'>
+      {!isMainPage && (
+        <div className='appbg'>
+          <div className='outline'></div>
+          <div className='inline'></div>
+          <div className='bgs'></div>
+          <Header />
+          <div className='flex'>
+            <Aside showPhotoAlbum={showPhotoAlbum} /> {/* Pass showPhotoAlbum state */}
+            <div className='content'>
+              <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/board" element={<Board />} />
+                <Route path="/guest" element={<Guest />} />
+                <Route path="/photo" element={<Photo />} />
+                <Route path="/photo/:nav" element={<Photo />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/photo/photoWrite" element={<PhotoWrite />} />
+                <Route path='/photo/photoReWrite/:id' element={<PhotoReWrite />} />
+              </Routes>
+            </div>
+            <Nav setShowPhotoAlbum={setShowPhotoAlbum} /> {/* Pass setShowPhotoAlbum function */}
+          </div>
+        </div>
+      )}
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path='/join' element={<Join />} />
+        <Route path='/find' element={<Find />} />
+      </Routes>
+    </div>
+  );
+};
+
+const App = () => {
   return (
     <Router>
-      <div className='app'>
-        <div className='outline'></div>
-        <div className='inline'></div>
-        <div className='bgs'></div>
-        <Header />
-        <div className='flex'>
-          <Aside showPhotoAlbum={showPhotoAlbum} /> {/* Pass showPhotoAlbum state */}
-          <div className='content'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/board" element={<Board />} />
-              <Route path="/guest" element={<Guest />} />
-              <Route path="/photo" element={<Photo />} />
-              <Route path="/photo/:nav" element={<Photo />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/photo/photoWrite" element={<PhotoWrite />} />
-              <Route path='/photo/photoReWrite/:id' element={<PhotoReWrite />} />
-              <Route path='/diary' element={<Diary />} />
-            </Routes>
-          </div>
-          <Nav setShowPhotoAlbum={setShowPhotoAlbum} /> {/* Pass setShowPhotoAlbum function */}
-        </div>
-      </div>
+      <AppContent />
     </Router>
   );
 };
